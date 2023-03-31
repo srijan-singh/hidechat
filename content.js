185df28fbca60805796d1af6314cae0dc29cd3ba
srijan-singh <srijanssj5goku@gmail.com>
2023-03-31 22:38:27 +0530

    Version 1.2 : Optimized Blurring

--- content.js
+++ content.js
@@ -2,19 +2,19 @@ console.log("Waiting for page to load...");
 
 // Function to hide the span element
 function hideSpanElement(spanElement) {
-  // Hide the span element by setting its opacity property to 0
+  // Hide the span element by blurring it
   spanElement.style.filter = 'blur(5px)';
 
   // Add a mouseover event listener to the parent element
   spanElement.parentElement.addEventListener("mouseover", function() {
-    // Show the span element by setting its opacity property to 1
+    // Show the span element by removing blur
     spanElement.style.filter = 'none';
   });
 
   // Add a mouseout event listener to the parent element
   spanElement.parentElement.addEventListener("mouseout", function() {
-    // Hide the span element by setting its opacity property to 0
-    spanElement.style.opacity = 0;
+    // Hide the span element by blurring it again
+    spanElement.style.filter = 'blur(5px)';
   });
 }
 
@@ -22,21 +22,22 @@ function hideSpanElement(spanElement) {
 window.addEventListener("load", function() {
   console.log("Page loaded.");
 
-  // Wait for an additional 10 seconds before hiding the span element
-  setTimeout(function() {
-    // Select all the span elements you want to hide using their class
-    const spanElements = document.querySelectorAll('span.ggj6brxn.gfz4du6o.r7fjleex.g0rxnol2.lhj4utae.le5p0ye3.l7jjieqr._11JPr');
+  // Set up a mutation observer to detect when span elements are added to the DOM
+  const observer = new MutationObserver(function(mutationsList) {
+    for (let mutation of mutationsList) {
+      if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+        // Select all the added span elements using their class
+        const addedSpanElements = document.querySelectorAll('span.ggj6brxn.gfz4du6o.r7fjleex.g0rxnol2.lhj4utae.le5p0ye3.l7jjieqr._11JPr');
 
-    // Check if the span elements were found
-    if (spanElements.length > 0) {
-      console.log(spanElements.length + " span elements found.");
-      // Loop through each span element and hide it
-      spanElements.forEach(function(spanElement) {
-        hideSpanElement(spanElement);
-      });
-    } else {
-      // Display an alert message if the span element was not found
-      console.log('Could not find the span element!');
+        // Loop through each added span element and hide it
+        addedSpanElements.forEach(function(addedSpanElement) {
+          hideSpanElement(addedSpanElement);
+        });
+      }
     }
-  }, 10000);
+  });
+
+  // Start observing the body element for changes to its child nodes
+  observer.observe(document.body, { childList: true, subtree: true });
+
 });
