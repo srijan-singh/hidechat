672f2d7996bf69bfe7c11bf22a39a6601817e0e5
srijan-singh <srijanssj5goku@gmail.com>
2023-03-31 21:51:59 +0530

    Version 1.0 : Hides but takes10 sec.

--- content.js
+++ content.js
@@ -6,20 +6,31 @@ window.addEventListener("load", function() {
 
   // Wait for an additional 5 seconds before hiding the span element
   setTimeout(function() {
-    // Select the span element you want to hide using its class
+    // Select all the span elements you want to modify using their class
     const spanElements = document.querySelectorAll('span.ggj6brxn.gfz4du6o.r7fjleex.g0rxnol2.lhj4utae.le5p0ye3.l7jjieqr._11JPr');
 
-    // Check if the span element was found
-     // Check if any span elements were found
-     if (spanElements.length > 0) {
-      console.log(spanElements.length + " span elements found.");
-      // Loop over the span elements and hide each one by setting its display property to 'none'
-      spanElements.forEach(function(spanElement) {
-        spanElement.style.display = 'none';
+    // Check if the span elements were found
+    if (spanElements.length > 0) {
+      console.log("Span elements found.");
+
+      // Loop through all the span elements and add the blur effect and hover behavior
+      spanElements.forEach(spanElement => {
+        // Set the CSS styles to blur the span element
+        spanElement.style.filter = 'blur(5px)';
+
+        // Show the span element on hover
+        spanElement.addEventListener('mouseover', () => {
+          spanElement.style.filter = 'none';
+        });
+
+        // Hide the span element on mouseout
+        spanElement.addEventListener('mouseout', () => {
+          spanElement.style.filter = 'blur(5px)';
+        });
       });
     } else {
-      // Display an alert message if the span element was not found
-      console.log('Could not find the span element!');
+      // Display an alert message if the span elements were not found
+      console.log('Could not find the span elements!');
     }
-  }, 5000);
+  }, 10000);
 });
